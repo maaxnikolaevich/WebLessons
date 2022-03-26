f7e05c34c651379af9251ae0a8ff72d68f4c019d
Maks <lebedkom03@gmail.com>
2022-03-26 14:38:09 +0500

    3 практика

--- components/list-items.js
+++ components/list-items.js
@@ -1,3 +1,33 @@
+const structPage={
+    listItem:{
+        tag:'div',
+        attr:'data-parent',
+        class:'list-item',
+        moreAttr:'list-item-open'
+    },
+    listItemInner:{
+        tag:'div',
+        class:'list-item__inner'
+    },
+    listItemItems:{
+        tag:'div',
+        class:'list-item__items'
+    },
+    imgArrow:{
+        tag:'img',
+        class:'list-item__arrow',
+        src:'./assets/img/chevron-down.png',
+        alt:'chevron-down',
+        attr:'data-open'
+    },
+    imgFolder:{
+        tag:'img',
+        class:'list-item__folder',
+        src:'./assets/img/folder.png',
+        alt:'folder'
+    }
+}
+
 export default class ListItems{
     constructor(el,data) {
         this.el=el;
@@ -18,29 +48,58 @@ export default class ListItems{
         parent.classList.toggle('list-item_open');
     }
     render(){
-        this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data));
+        this.renderParent(this.data)
+        // this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data));
     }
     
     renderParent(data) {
+        let newSpan=document.createElement('span');
+        let newItemElem=this.renderStructItem(structPage.listItem)
+        let newInnerElem=this.renderStructItem(structPage.listItemInner)
+        newSpan.innerHTML=data.name;
         if (data.hasChildren){
-            let currentNode="<div class=\"list-item list-item_open\" data-parent>\n"+
-                "<div class=\"list-item__inner\">\n"+
-                "<img class=\"list-item__arrow\" src=\"./assets/img/chevron-down.png\" alt=\"chevron-down\""+
-                " data-open>\n" +"<img class=\"list-item__folder\"" +
-                " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+"<span>"+data.name+"</span>"
-                +"</div>\n"+"<div class=\"list-item__items\">"
-            
+            newInnerElem.append(this.renderStructItem(structPage.imgArrow),this.renderStructItem(structPage.imgFolder),newSpan)
+            newItemElem.append(newInnerElem)
+            this.el.append(newItemElem)
+        //     let currentNode="<div class=\"list-item list-item_open\" data-parent>\n"+
+        //         "<div class=\"list-item__inner\">\n"+
+        //         "<img class=\"list-item__arrow\" src=\"./assets/img/chevron-down.png\" alt=\"chevron-down\""+
+        //         " data-open>\n" +"<img class=\"list-item__folder\"" +
+        //         " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+"<span>"+data.name+"</span>"
+        //         +"</div>\n"+"<div class=\"list-item__items\">"
+        //    
             data.items.forEach(node=>{
-                currentNode+=this.renderParent(node);
+                this.renderParent(node);
             });
-            return currentNode+"</div> </div>"
+        //     return currentNode+"</div> </div>"
         }   
         else
         {
-            return "<div class=\"list-item__inner\">\n"+
-            "<img class=\"list-item__folder\"" +
-                " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+ 
-                "<span>"+data.name+"</span>\n"+"</div>"
+            newInnerElem.append(this.renderStructItem(structPage.imgFolder),newSpan)
+            newItemElem.append(newInnerElem)
+            this.el.append(newItemElem)
+            // return "<div class=\"list-item__inner\">\n"+
+            // "<img class=\"list-item__folder\"" +
+            //     " src=\"./assets/img/folder.png\" alt=\"folder\">\n"+ 
+            //     "<span>"+data.name+"</span>\n"+"</div>"
         }
+    }
+    // renderInnerElem(name){
+    //     let newInnerElem=this.renderStructItem(structPage.listItemInner)
+    //     newInnerElem.append(this.renderStructItem(structPage.imgArrow),this.renderStructItem(structPage.imgFolder),newSpan)
+    //     newItemElem.append(newInnerElem)
+    //     this.el.append(newItemElem)
+    // }
+    renderStructItem(structItem){
+        let newItem=document.createElement(structItem.tag);
+        newItem.className=structItem.class;
+        if (structItem.moreAttr!==undefined) newItem.classList.add(structItem.moreAttr);
+        if (structItem.tag === 'img'){
+            newItem.src=structItem.src;
+            newItem.alt=structItem.alt;
+            return newItem
+        }
+        if (structItem.attr!==undefined) newItem.setAttribute(structItem.attr,'');
+        return newItem
     }
 } 
